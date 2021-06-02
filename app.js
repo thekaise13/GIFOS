f45321746204e7304bd57be49657d7373697e626
cristian caicedo <cristian.caicedop@gmail.com>
2021-06-01 22:49:59 -0500

    innerText modo diurno

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-
-
+//modo nocturno
 const nocturno = document.querySelector('.nocturno')
 
 nocturno.onclick = () => {
     const img = document.querySelector('.logo')
     const crear = document.querySelector('.crearimg')
+    const modo = document.querySelector('.modo')
     document.body.classList.toggle('dark-theme')
     if (document.body.classList.contains('dark-theme')) {
         img.src = "images/Logo-modo-noc.svg"
         crear.src = "images/CTA-crar-gifo-modo-noc.svg"
+        modo.innerText = "Modo Diurno"
     } else {
         img.src = "images/logo-mobile.svg"
         crear.src = "images/button-crear-gifo.svg"
+        modo.innerText = "Modo Nocturno"
     }
 }
+
+
